9c7570efa587a3f30c24dc956b8d3025e2f21ee5
vera-bernhard <vera.l.bernhard@bluewin.ch>
2025-03-20 11:40:04 +0100

    Update settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "psynamic")
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+)
