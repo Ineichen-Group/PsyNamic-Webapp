9767e5382c4c24c1f08e13fb4efd591e01f07564
vera-bernhard <vera.l.bernhard@bluewin.ch>
2025-03-20 19:19:40 +0100

    readd study display

--- pages/insights/views.py
+++ pages/insights/views.py
@@ -25,8 +25,7 @@ def view_layout(title: str, graph: dcc.Graph, filter_buttons: list[dbc.Button], 
         graph,
         html.H4("Filtered Studies"),
         filter_component(filter_buttons),
-        # studies_display(tag_data)
-        # studies_display(study_tags)
+        studies_display(study_tags)
     ])
 
 
